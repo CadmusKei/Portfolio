b8b50fd60a492c0c8669b213269d1701f3b11d00
Kei <maxwell.kei.farouk@gmail.com>
2025-11-30 22:13:12 +0200

    Changed method

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,9 +13,9 @@ function Nav() {
       <div className="w-full h-25 bg-[#1111118e] fixed shadow-xl z-50">
         <div className="flex pl-10 w-full h-full items-center space-x-20">
           <div className="flex items-center justify-center w-25 h-20"><img src={iconPath} alt="" className="h-[80%] w-[80%]" /></div>
-          <div className="flex items-baseline mt-2">
+          <div className="flex w-full items-center mt-2">
             <Title className="pl-1 underline" name="Kei Farouk"/>
-            <div className="flex space-x-20 ml-85">
+            <div className="flex space-x-20 ml-[40%]">
               <Icon path={piIcon} scalar="w-8 h-6 " hoverColour="hover:border-emerald-200 hover:bg-emerald-800" />  
               <Icon path={chipIcon} scalar="w-8 h-8 " hoverColour="hover:border-purple-400 hover:bg-purple-900"   />  
               <Icon path={quaverIcon} scalar="w-7 h-6 " hoverColour="hover:border-cyan-200  hover:bg-cyan-600 "/>  
