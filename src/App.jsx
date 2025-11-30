e859bc5345c2a813c7e2367184c64f8065b7e795
Kei <maxwell.kei.farouk@gmail.com>
2025-11-30 20:05:55 +0200

    Centered posters

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
       <Nav />
       <div
         ref={scrollRef}
-        className="flex space-x-30 w-full h-screen bg-[#212121] overflow-x-hidden  overflow-y-hidden"
+        className="flex space-x-30 w-full h-screen bg-[#212121] overflow-x-hidden overflow-y-hidden justify-center items-center"
       >
         <Poster name="Bio" desc="Who is this guy?!" image={posterImage} className="mt-40"/>
         <Poster name="Projects" desc="A peak inside the Github vault"  image={CSImage} className="mt-40"/>
