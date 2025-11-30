957f53173725fc58fed88675d533d34b04bd3254
Kei <maxwell.kei.farouk@gmail.com>
2025-11-30 20:13:05 +0200

    Adjusted poster sizes for bigger screens

--- src/components/Poster.jsx
+++ src/components/Poster.jsx
@@ -8,7 +8,7 @@ function Poster({className, name, desc, image}) {
   return (
     <>
       {/* Poster dividing blocks */}
-      <div className={`flex w-[90rem] flex-shrink-0 h-140 justify-center space-x-30 mt-30 rounded-4xl bg-[#11111183] shadow-2xl`}>
+      <div className={`flex w-[96%] flex-shrink-0 h-140 justify-center space-x-30 mt-30 rounded-4xl bg-[#11111183] shadow-2xl`}>
         <Block width="w-108" height="h-140" className="shadow-2xl -rotate-2 hover:rotate-2 transition duration-700">
           <div className="flex flex-col h-full w-full items-center">
             <AnimatedCover src={image} height="h-116" width="w-full"/>
