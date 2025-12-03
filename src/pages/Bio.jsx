febae95656b1a08437c289dc8a756a18014e671b
Kei <maxwell.kei.farouk@gmail.com>
2025-12-03 21:43:41 +0200

    Content Editing

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -15,10 +15,13 @@ function Bio(){
             <Nav TitleColour="text-white" background={pageGradient}/>
             <div className={`flex flex-col pl-20 w-full h-400 bg-cover `} style={{backgroundImage: `url(${backgroundPic})`}}>
 
-                <div className={`flex w-[42%] flex-col h-150 ${pageGradient} mt-40 rounded-lg border-4 border-neutral-400 p-12 space-y-4 shadow-2xl shadow-black`} >
-                        <Text>I am a Computer Science student at The Univeristity of The Western Cape. I am a developer, musician and general creative. </Text>
-                        <Text>I am a Computer Science student at The Univeristity of The Western Cape. I am a developer, musician and general creative. </Text>
-                        <Text>I am a Computer Science student at The Univeristity of The Western Cape. I am a developer, musician and general creative. </Text>
+                <div className={`flex w-[42%] flex-col h-[35%] ${pageGradient} mt-40 rounded-2xl 
+                p-12 justify-start items-center space-y-4 shadow-2xl shadow-black`} >
+
+                        <Text>I am a Computer Science student at The Univeristity of The Western Cape. I am a Developer, Musician and General-Creative. </Text>
+                        <Text>I endavour to mix my passions for Math, Computer-Science, Music and Art in all the ways I can imagine.</Text>
+                        <Text>With experience in  Web Development, Game Development, 3D-Modeling, Music Production and so much more, I aspire to bring my visions to life.</Text>
+                         <Text> </Text>
 
                 </div>
 
