4377beeadb9b746cf1eecc2f668cdf51a2aa13e0
Kei <maxwell.kei.farouk@gmail.com>
2025-12-03 22:14:25 +0200

    Bio Changes

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -1,13 +1,9 @@
 import Nav from "../components/Nav";
-import Profile from "../components/Profile";
 import Title from "../components/Title";
-import Description from "../components/Description";
 import Text from "../components/Text";
 
-import profilePic from '../assets/Images/me.png';
 import backgroundPic from '../assets/Images/studio-site-crop.jpeg';
-
-    const pageGradient = "bg-linear-to-br from-neutral-950 to-[#28282841] "
+const pageGradient = "bg-linear-to-br from-neutral-950 to-[#28282841] "
 
 function Bio(){
     return (
@@ -16,13 +12,16 @@ function Bio(){
             <div className={`flex flex-col pl-20 w-full h-400 bg-cover `} style={{backgroundImage: `url(${backgroundPic})`}}>
 
                 <div className={`flex w-[42%] flex-col h-[35%] ${pageGradient} mt-40 rounded-2xl 
+                p-12 justify-start items-center space-y-4 shadow-2xl hover:to-[#07655841] transition duration-800 shadow-black`} >
+                        <Text>I am a Computer Science student at the University of the Western Cape — a developer, musician, and all-round creative.</Text>
+                        <Text>I love blending my passions for technology, mathematics, music, and art to build projects that come alive.</Text>
+                        <Text>With experience in web development, game development, 3D modelling, and music production, I’m driven to bring ideas to life across disciplines. </Text>
+                        <Text>Some of my other passions include: HPC, app development, simulations, Lo-fi Production and VFX. </Text>
+                </div>
+
+                <div className={`flex w-[95%] flex-col h-[35%] ${pageGradient} mt-50 rounded-2xl 
                 p-12 justify-start items-center space-y-4 shadow-2xl shadow-black`} >
-
-                        <Text>I am a Computer Science student at The Univeristity of The Western Cape. I am a Developer, Musician and General-Creative. </Text>
-                        <Text>I endavour to mix my passions for Math, Computer-Science, Music and Art in all the ways I can imagine.</Text>
-                        <Text>With experience in  Web Development, Game Development, 3D-Modeling, Music Production and so much more, I aspire to bring my visions to life.</Text>
-                         <Text> Something hinting to me wanting to work on space</Text>
-
+                        <Title className="text-white" name="Experience"></Title>
                 </div>
 
                 <div className="bg-[#28282841]"></div>
