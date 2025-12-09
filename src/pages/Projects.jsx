7cdcb80c931b19fbb4e5eb36952093a0614fbafc
Kei <maxwell.kei.farouk@gmail.com>
2025-12-09 18:17:43 +0200

    rearanged some items

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,7 +9,7 @@ import githubLogo from "../assets/Icons/Tools/Github.svg"
 import arrowIcon from "../assets/Icons/Down-Arrow.svg"
 
 import suikaImage from "../assets/Images/Projects/Suika.png"
-import verifierImage from "../assets/Images/Projects/Verifier_2.png"
+import verifierImage from "../assets/Images/Projects/Verifier.png"
 import nsdImage from "../assets/Images/Projects/nsd.png"
 
 function Projects() {
@@ -24,7 +24,7 @@ function Projects() {
   
 
         <div className="relative z-10 scrollbar">
-          <ProjectSection imageSrc={verifierImage} scale="h-[80%]" colours="bg-linear-to-br from-[#811964] to-[#110b39]">
+          <ProjectSection imageSrc={verifierImage} scale="w-[80%]" colours="bg-linear-to-br from-[#811964] to-[#110b39]">
               <div className="h-[0.2rem] w-full bg-[#ffffff5c] rounded-full"></div>
               <Title className="text-white text-8xl underline underline-offset-10" name="Verifier" />  
               <Text>As a team, developed a qualification verifier using a feature-rich 
@@ -36,12 +36,32 @@ function Projects() {
                 scalar="w-13 h-13" buttonSize="w-20 h-20" target="_blank"
                 hoverColour="border-pink-200 hover:border-[#df40d45d]
                   to-[#7a34d6c3] from-[#811964]"/>
-                <Icon path={arrowIcon} link="#suika" scalar="w-10 h-6 "
+                <Icon path={arrowIcon} link="#nsd" scalar="w-10 h-6 "
                  buttonSize="w-20 h-20" hoverColour="border-pink-200 hover:border-[#df40d45d]
                   to-[#7a34d6c3] from-[#811964]"/>  
               </div>
               <div className="h-[0.2rem] w-full bg-[#ffffff5c] rounded-full"></div>
           </ProjectSection>
+        </div>
+
+        <div className="w-full h-screen">
+        <a id="nsd"></a>
+          <ProjectSection imageSrc={nsdImage} scale="w-[90%]" colours="bg-linear-to-br from-[#811964] to-[#110b39]">    
+              <div className="h-[0.2rem] w-full bg-[#ffffff5c] rounded-full"></div>
+              <Title className="text-white text-8xl underline underline-offset-10 " name="NSD-RUN!" />
+              <Text>Pixel-Art clone of popular Chinese game 'Suika Game' made
+                  using python game development library Pygame, and python's physics
+                  simulation engine pymunk.</Text>
+              <div className="flex w-full justify-around">
+                <Icon path={githubLogo} link="https://github.com/CadmusKei/NSD-RUN" target="_blank" scalar="w-13 h-13" buttonSize="w-20 h-20"
+                hoverColour="border-pink-200 hover:border-[#df40d45d]
+                  to-[#7a34d6c3] from-[#811964]"/>
+                <Icon path={arrowIcon} link="#suika" scalar="w-10 h-6 "
+                 buttonSize="w-20 h-20" hoverColour="border-pink-200 hover:border-[#df40d45d]
+                  to-[#7a34d6c3] from-[#811964]" />  
+              </div>
+              <div className="h-[0.2rem] w-full bg-[#ffffff5c] rounded-full"></div>
+          </ProjectSection>    
         </div>
 
         <div className="w-full h-screen">
@@ -56,7 +76,7 @@ function Projects() {
                 <Icon path={githubLogo} link="https://github.com/CadmusKei/Suika-Game-Clone" target="_blank" scalar="w-13 h-13" buttonSize="w-20 h-20"
                 hoverColour="border-pink-200 hover:border-[#df40d45d]
                   to-[#7a34d6c3] from-[#811964]"/>
-                <Icon path={arrowIcon} link="#nsd" scalar="w-10 h-6 "
+                <Icon path={arrowIcon} link="#" scalar="w-10 h-6 "
                  buttonSize="w-20 h-20" hoverColour="border-pink-200 hover:border-[#df40d45d]
                   to-[#7a34d6c3] from-[#811964]" />  
               </div>
@@ -64,25 +84,7 @@ function Projects() {
           </ProjectSection>    
         </div>
       
-      <div className="w-full h-screen">
-        <a id="nsd"></a>
-          <ProjectSection imageSrc={nsdImage} scale="w-[90%]" colours="bg-linear-to-br from-[#811964] to-[#110b39]">    
-              <div className="h-[0.2rem] w-full bg-[#ffffff5c] rounded-full"></div>
-              <Title className="text-white text-8xl underline underline-offset-10 " name="NSD-RUN!" />
-              <Text>Pixel-Art clone of popular Chinese game 'Suika Game' made
-                  using python game development library Pygame, and python's physics
-                  simulation engine pymunk.</Text>
-              <div className="flex w-full justify-around">
-                <Icon path={githubLogo} link="https://github.com/CadmusKei/NSD-RUN" target="_blank" scalar="w-13 h-13" buttonSize="w-20 h-20"
-                hoverColour="border-pink-200 hover:border-[#df40d45d]
-                  to-[#7a34d6c3] from-[#811964]"/>
-                <Icon path={arrowIcon} link="#nsd" scalar="w-10 h-6 "
-                 buttonSize="w-20 h-20" hoverColour="border-pink-200 hover:border-[#df40d45d]
-                  to-[#7a34d6c3] from-[#811964]" />  
-              </div>
-              <div className="h-[0.2rem] w-full bg-[#ffffff5c] rounded-full"></div>
-          </ProjectSection>    
-        </div>
+      
       
 
     </>
