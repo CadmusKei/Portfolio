929f602c58f3bab3327a7a82b921cc59ea684b43
Kei <maxwell.kei.farouk@gmail.com>
2025-12-11 00:35:17 +0200

    Refactor Music page layout and update styling for improved UI and content

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -10,7 +10,7 @@ import me_2 from "../assets/Images/Music/me_2.jpeg"
 
 export default function Music() {
 
-    const mainUI = "bg-[#2121218e]"
+    const mainUI = "#2121218e"
 
     return (
 
@@ -28,22 +28,25 @@ export default function Music() {
                 </div>
 
                 
-                <div className="flex flex-col items-center w-full h-full bg-black">
+                <div className="flex flex-col items-center w-full h-400 bg-black">
                     
-                    <div className="mt-15 p-10 space-x-10 flex w-[95%] h-[85%] bg-[#111] rounded-4xl">
+                    <div className="mt-20 p-10 space-x-10 flex w-[95%] h-[80%] bg-[#111] rounded-4xl">
                         
 
-                        <div className={`w-[80%] h-[90%] ${mainUI} rounded-4xl`}>
-
+                        <div className={`flex flex-col space-y-10 p-10 w-[70%] h-[100%] bg-[${mainUI}] rounded-4xl`}>
+                            <Title name="Samples" className="text-white text-8xl underline"/>
+                            <Text>Find here a few samples of Cadmus's work.</Text>
                         </div>
 
 
-                        <div className={`w-[25%] h-[80%] rounded-4xl ${mainUI} overflow-hidden`}>
+                        <div className={`flex flex-col w-[30%] h-[70%] rounded-4xl bg-[${mainUI}] overflow-hidden shadow-2xl shadow-black`}>
                             <div className={`relative flex items-end w-full h-[60%] bg-cover bg-bottom`} style={{ backgroundImage: `url(${me_2})` }}> 
-                                <div className="absolute inset-0 bg-linear-to-t from-black to-[#ffffff00]"></div>
+                                <div className={`absolute inset-0 bg-linear-to-t from-[#000] to-transparent`}></div>
                                 <Text className="text-white z-10 m-5 text-3xl">About Cadmus</Text> 
                             </div>  
-                            <Text className="text-white z-10 m-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta, repellat.</Text> 
+                            <Text className="text-white z-10 m-5">A South African Lo-fi and Jazz Hip-Hop producer. 
+                                Taking inspiration from both classical hip-hop producers and lo-fi pioneers by the likes of Nujabes.  
+                                Blending a classical piano background with experimental vocals.</Text> 
                         </div>
                         
                     </div>
