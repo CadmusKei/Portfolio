f07af3f5a16f6198a7a197c43e11475a7ee81b6b
Kei <maxwell.kei.farouk@gmail.com>
2025-12-11 16:00:52 +0200

    Add functionality to AudioPlayer, including cycle features, play, pause and display Title

--- src/components/AudioButton.jsx
+++ src/components/AudioButton.jsx
@@ -1,11 +1,11 @@
 import playIcon from '../assets/Icons/audio/Play.svg'
 
-export default function AudioButton({icon}) {
+export default function AudioButton({icon, onClick}) {
     return (
-        <div className="flex items-center justify-center w-25 h-25 bg-[#393939] p-3
+        <button onClick={onClick} className="flex items-center justify-center w-25 h-25 bg-[#393939] p-3
         active:shadow-sm shadow-lg transition-all rounded-2xl shadow-[#111] duration-75 border-8
-         border-[#2d2c2c]">
+         border-[#2f2f2f]">
                 <img className='w-[90%] h-[90%]' src={icon} alt="" />
-        </div>
+        </button>
     );
 }
--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -1,45 +1,97 @@
 
-import AudioButton from "./AudioButton"
-import Title from "./Title"
-import Text from "./Text"
+    import { useRef, useEffect, useState } from "react";
 
-import playIcon from '../assets/Icons/audio/Play.svg'
-import pauseIcon from '../assets/Icons/audio/Pause.svg'
-import nextIcon from '../assets/Icons/audio/Next.svg'
-import lastIcon from '../assets/Icons/audio/Last.svg'
+    import AudioButton from "./AudioButton"
+    import Title from "./Title"
+    import Text from "./Text"
 
+    import playIcon from '../assets/Icons/audio/Play.svg'
+    import pauseIcon from '../assets/Icons/audio/Pause.svg'
+    import nextIcon from '../assets/Icons/audio/Next.svg'
+    import lastIcon from '../assets/Icons/audio/Last.svg'
 
+    import slowlyPath from '../assets/Demos/Slowly.mp3'
+    import iverPath from '../assets/Demos/Iver.mp3'
+    import wishICouldBePath from '../assets/Demos/Wish I Could Be the Reason Why.mp3'
 
-export default function AudioPlayer() {
+    export default function AudioPlayer() {
 
-    const demoList = {};
+        const demoObject = {"Slowly": {Title: "Slowly", Path: slowlyPath}, 
+                        "Iver": {Title: "Iver", Path: iverPath},
+                        "WishICouldBe": {Title: "Which I Could Be The Reason", Path: wishICouldBePath}, 
+                    };
 
-    return (
+        const demoArray = [
+            demoObject.Slowly,
+            demoObject.Iver,
+            demoObject.WishICouldBe
+        ];
+                        
+        let [index, setIndex] = useState(0);
+        const [currentTrack, setCurrentTrack] = useState(demoObject.Slowly.Path); 
+        const audioRef = useRef(null);
 
-        <div className={`flex flex-col items-center space-y-8 p-10 w-[70%] h-[85%] 
-        bg-[#363636] rounded-4xl border-8 border-[#474747] bg-linear-to-br from-[#aaaaaa] via-[#969696] via-20% to-[#5c5c5c] `}>
-                            
-            <div className="flex items-center justify-between w-full h-[10%] pl-5 pr-10">
-                <Title name="Cad-Sample" className=" text-black"/> 
-                <div className="w-10 h-10 rounded-md bg-[#232323] shadow-md shadow-[#262626]"></div>   
+        function playNext()
+        {
+            if (index < demoArray.length - 1)
+            {
+                setIndex(index + 1);
+            }
+            else
+            {
+                setIndex(0);
+            }      
+        }
+
+        function playLast()
+        {
+            if (index > 0)
+            {
+                setIndex(index - 1);
+            }
+            else 
+            {
+                setIndex(demoArray.length - 1);
+            }
+
+        }
+
+        useEffect(() => {
+            setCurrentTrack(demoArray[index].Path);
+        }, [index] );
+
+        useEffect(() => {
+            audioRef.current.play();
+        }, [currentTrack] );
+
+        return (
+
+            <div className={`flex flex-col items-center space-y-8 p-10 w-[70%] h-[85%] 
+            bg-[#363636] rounded-t-4xl rounded-b-xl border-8 border-[#525252] bg-linear-to-br 
+            from-[#cecece] via-[#969696] via-20% to-[#5c5c5c] shadow-2xl shadow-black `}>
+                                
+                <div className="flex items-center justify-between w-full h-[10%] pl-5 pr-10">
+                    <Title name="Cad-Sample" className=" text-black"/>  
+                    <div className="w-10 h-10 rounded-md bg-[#232323] shadow-md shadow-[#262626]"></div>   
+                </div>
+
+                <div className="flex items-center justify-center w-full h-[50%] bg-linear-to-br from-[#323232] 
+                via-[#131313] via-30% to-[#02090e] rounded-4xl shadow-xl shadow-[#282828]">
+
+                    <audio ref={audioRef} src={currentTrack}></audio>
+                    <Text className="font-bold text-4xl">{demoArray[index].Title}</Text>
+
+                </div>
+
+                <div className="flex items-center justify-around h-[30%] w-[65%]">
+                    <AudioButton onClick={() => playLast()} icon={lastIcon} />
+                    <AudioButton onClick={() => audioRef.current.play()} icon={playIcon}/>
+                    <AudioButton onClick={() => audioRef.current.pause()} icon={pauseIcon}/>
+                    <AudioButton onClick={() => playNext()} icon={nextIcon}/>
+                </div>
+
             </div>
 
-            <div className="flex items-center justify-center w-full h-[50%] bg-linear-to-br from-[#323232] 
-            via-[#131313] via-30% to-[#02090e] rounded-4xl shadow-xl shadow-[#282828]">
+        );
 
-                <Text className="font-bold text-4xl">This is a test</Text>
-
-            </div>
-
-            <div className="flex items-center justify-around h-[30%] w-[65%]">
-                <AudioButton icon={lastIcon}/>
-                <AudioButton icon={playIcon}/>
-                <AudioButton icon={pauseIcon}/>
-                <AudioButton icon={nextIcon}/>
-            </div>
-
-        </div>
-
-    );
-
-}
+    }
