123c22980e1175572d07e95e46952700b3709d71
CadmusKei <maxwell.kei.farouk@gmail.com>
2026-04-18 02:59:34 +0200

    Add backdrop blur effect to Description component for enhanced visual depth; fix border styling in Block component

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -3,7 +3,7 @@
 function Block({ children, className, hoverColour }) {
     return (
         <div className={`flex flex-col md:flex-row w-full md:w-[96%] shrink-0 h-[80%] md:h-[65%] justify-center items-center 
-          md:space-x-30 space-y-10 rounded-4xl bg-linear-to-br from-neutral-950 -[#41414141] transition duration-800 ${hoverColour} shadow-2xl ${className}`}>{children}
+          md:space-x-30  border border-white/15 space-y-10 rounded-4xl bg-linear-to-br from-neutral-950 to-white/2 transition duration-800 ${hoverColour} shadow-2xl ${className}`}>{children}
         
           </div>
 
--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,5 +1,5 @@
 function Description({ content }) {
-  return (<div className="flex text-center items-center bg-[#34343452] p-6 rounded-4xl  md:rounded-full shadow-xl">
+  return (<div className="flex text-center items-center backdrop-blur-2xl bg-[#34343452] p-6 rounded-4xl  md:rounded-full shadow-xl">
     <p className=" flex text-white text-3xl md:text-5xl text-pixel  ">{content}</p>
   </div>);
 
